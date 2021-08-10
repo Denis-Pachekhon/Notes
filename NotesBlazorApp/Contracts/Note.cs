1dc1c4863db58a5cf01a72e3ac76ee6ed62a5604
DenisPachekhon <denis.pachekhon.work@gmail.com>
2021-08-10 18:17:06 +0300

    Add validation

--- NotesBlazorApp/Contracts/Note.cs
+++ NotesBlazorApp/Contracts/Note.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace NotesBlazorApp.Contracts
 {
     public class Note
     {
         public int Id { get; set; }
+
+        [StringLength(50, ErrorMessage = "Exceeding the maximum number of characters")]
         public string Title { get; set; }
+
+        [StringLength(3000, ErrorMessage = "Exceeding the maximum number of characters")]
         public string Text { get; set; }
+
         public DateTime DateOfCreation { get; set; }
     }
 }
